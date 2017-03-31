e300b198af6c37460eb2a619f460e847f5c8705f
0xHJK <HJKdev+GitHub@gmail.com>
2017-03-31 17:16:54 +0800

    模版生成测试完成

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from test import UserCtrl, get_users
+from flask import Flask, render_template
+# from test import UserCtrl, get_users
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    u1 = UserCtrl('tom')
-    u2 = UserCtrl('jack')
-    return ' '.join(get_users())
+    # u1 = UserCtrl('tom')
+    # u2 = UserCtrl('jack')
+    # return ' '.join(get_users())
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
